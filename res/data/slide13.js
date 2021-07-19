20a5125e1e47e2b2d9613eb25ab6a6b99a05fc35
KhloutSambath <65694832+KhloutSambath@users.noreply.github.com>
2021-07-20 05:10:17 +0700

    interactive

---
+++ res/data/slide13.js
@@ -0,0 +1,8 @@
+(function(){var loadHandler=window['sl_{48A7EC03-237D-4E6C-823C-5FD379FC4E0B}'];loadHandler&&loadHandler(12, '<div id="spr0_30e6dc"><div id="spr1_30e6dc" class="kern slide"><img id="img1_30e6dc" src="data/img0.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_30e6dc" class="kern slide"><div id="svg0_30e6dc" style="left:64px;top:26.75px;"><svg width="832" height="91" viewBox="-1.5 -1.5 832 91"><path fill="#ffffff" stroke="#ed7d31" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="8" d="M0.5,0.5 h828 v86.45 h-828 Z"/></svg></div><div id="spr3_30e6dc" style="left:65.5px;top:28.25px;"><div style="width:0px;"><span id="txt0_30e6dc" data-width="282.324219" style="left:294.438px;top:11.138px;">លំហាត់ពង្រឹងពុទ្ធិ</span></div></div><div id="svg1_30e6dc" style="left:65.25px;top:143px;"><svg width="830" height="345" viewBox="-0.375 -0.375 830 345"><path fill="none" stroke="#000000" stroke-width="0.8" stroke-linecap="butt" stroke-linejoin="round" d="M0.375,0.375 h828 v342.625 h-828 Z"/></svg></div><div id="spr4_30e6dc" style="left:75px;top:146px;"><img id="img0_30e6dc" src="data/img19.png" width="320" height="312" alt="គណនាដេរីវេនៃអនុគមន៍ខាងក្រោម\
+​𝑦=𝑥𝑡𝑎𝑛𝑥\
+ 𝑦= 𝑥 2 + tan  3𝑥+1  \
+ 𝑦=3 𝑥 2 − cot 2𝑥 \
+ 𝑦= cot  2 𝑥 2 −𝑥 − 𝑥 3  \
+ 𝑦= 𝑥 2  tan   𝑥 2 +𝑥+1  \
+ 𝑦= tan  𝑐𝑜𝑡𝑥+𝑥  \
+ 𝑦= sin  𝑡𝑎𝑛𝑥+𝑐𝑜𝑡𝑥  "/></div><div id="spr5_30e6dc" style="left:678px;top:500.5px;"><div style="width:0px;"><span id="txt1_30e6dc" data-width="12.164063" style="left:196.45px;top:5.657px;">13</span></div></div></div></div>');})();
